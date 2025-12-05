069389209637c21f5e54446b4c0363679c0030ad
Jonas Sävås <jonas.savas123@gmail.com>
2025-12-05 15:48:39 +0100

    Adds the solution to day 3 part 1

--- day3/main.cpp
+++ day3/main.cpp
@@ -3,6 +3,8 @@
 #include <string>
 #include <vector>
 
+int mostJoltage(const std::string &s);
+
 int main(){
 
     std::ifstream file("../input.txt");
@@ -12,14 +14,48 @@ int main(){
     }
 
     std::string line;
+    unsigned long long sumJoltage = 0;
 
     while(std::getline(file, line)){
-        
-
+        int currJoltage = mostJoltage(line);
+        //std::cout<< "CURR: " <<currJoltage <<std::endl;
+        sumJoltage += currJoltage;
     }
 
 
     file.close();
 
+    std::cout<< "Sum Joltage: " << sumJoltage << std::endl;
+
     return 0;
+}
+
+
+int mostJoltage(const std::string &s){
+    int firstNum = 0;
+    int secondNum = 0;
+    int firstNumIndex = 0;
+    for(int i = 0; i <= s.size() - 2; ++i){
+        // Find the max number
+        if(s[i]  - '0' > firstNum){
+            firstNum = s[i]  - '0';
+            firstNumIndex = i;
+        }
+    }
+    //std::cout<<"Found first num: " << firstNum << std::endl;
+    if(firstNumIndex == s.size() - 2){
+        secondNum = s[s.size() - 1] - '0';
+    }else{
+        int secondNumIndex = firstNumIndex + 1;
+        while(secondNumIndex <= s.size() - 1){
+            if(s[secondNumIndex] - '0' > secondNum){
+                secondNum = s[secondNumIndex] - '0';
+            }
+            secondNumIndex++;
+        }
+    }
+    //std::cout << "FOUND second num: " << secondNum << std::endl;
+    int result = firstNum*10 + secondNum;
+    return result;
+
 }
