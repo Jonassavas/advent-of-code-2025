484fa64f0d0fcaa786bcdb0ff2609636b486c8fc
Jonas Sävås <jonas.savas123@gmail.com>
2025-12-05 18:24:27 +0100

    Adds the outline files for each remaining day

---
+++ day9/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+
+int main(){
+    std::fstream file("../input.txt");
+
+    if(!file.is_open()){
+        std::cerr << "Could not open file. \n";
+        return 1;
+    }
+
+    std::string line;
+    while(std::getline(file, line)){
+
+
+    }
+
+    file.close();
+
+    return 0;
+}
