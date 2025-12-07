cf1d51ad9e83ebcef96133723c49b4bbd9a3773d
Jonas Sävås <jonas.savas123@gmail.com>
2025-12-07 23:15:15 +0100

    Adds the solution to day 6 part 1

--- README.md
+++ README.md
@@ -13,19 +13,19 @@ Day 3: [https://adventofcode.com/2025/day/3](https://adventofcode.com/2025/day/3
 Day 4: [https://adventofcode.com/2025/day/4](https://adventofcode.com/2025/day/4) --> Solution: [Part 1](./day4/main.cpp) & [Part 2](./day4/main2.cpp)
 
 Day 5: [https://adventofcode.com/2025/day/5](https://adventofcode.com/2025/day/5) --> Solution: [Part 1](./day5/main.cpp) & [Part 2](./day5/main2.cpp)
+
+Day 6: [https://adventofcode.com/2025/day/6](https://adventofcode.com/2025/day/6) --> Solution: [Part 1](./day6/main.cpp) & [Part 2](./day6/main2.cpp)
 <!--
-Day 6: [https://adventofcode.com/2025/day/6](https://adventofcode.com/2025/day/3) -/-> Solution: [Part 1](./day3/main.cpp) & [Part 2](./day3/main2.cpp)
+Day 7: [https://adventofcode.com/2025/day/7](https://adventofcode.com/2025/day/7) -/-> Solution: [Part 1](./day7/main.cpp) & [Part 2](./day7/main2.cpp)
 
-Day 7: [https://adventofcode.com/2025/day/7](https://adventofcode.com/2025/day/3) -/-> Solution: [Part 1](./day3/main.cpp) & [Part 2](./day3/main2.cpp)
+Day 8: [https://adventofcode.com/2025/day/8](https://adventofcode.com/2025/day/8) -/-> Solution: [Part 1](./day8/main.cpp) & [Part 2](./day8/main2.cpp)
 
-Day 8: [https://adventofcode.com/2025/day/8](https://adventofcode.com/2025/day/3) -/-> Solution: [Part 1](./day3/main.cpp) & [Part 2](./day3/main2.cpp)
+Day 9: [https://adventofcode.com/2025/day/9](https://adventofcode.com/2025/day/9) -/-> Solution: [Part 1](./day9/main.cpp) & [Part 2](./day9/main2.cpp)
 
-Day 9: [https://adventofcode.com/2025/day/9](https://adventofcode.com/2025/day/3) -/-> Solution: [Part 1](./day3/main.cpp) & [Part 2](./day3/main2.cpp)
+Day 10: [https://adventofcode.com/2025/day/10](https://adventofcode.com/2025/day/10) -/-> Solution: [Part 1](./day10/main.cpp) & [Part 2](./day10/main2.cpp)
 
-Day 10: [https://adventofcode.com/2025/day/10](https://adventofcode.com/2025/day/3) -/-> Solution: [Part 1](./day3/main.cpp) & [Part 2](./day3/main2.cpp)
+Day 11: [https://adventofcode.com/2025/day/11](https://adventofcode.com/2025/day/11) -/-> Solution: [Part 1](./day11/main.cpp) & [Part 2](./day11/main2.cpp)
 
-Day 11: [https://adventofcode.com/2025/day/11](https://adventofcode.com/2025/day/3) -/-> Solution: [Part 1](./day3/main.cpp) & [Part 2](./day3/main2.cpp)
-
-Day 12: [https://adventofcode.com/2025/day/12](https://adventofcode.com/2025/day/3) -/-> Solution: [Part 1](./day3/main.cpp) & [Part 2](./day3/main2.cpp)
+Day 12: [https://adventofcode.com/2025/day/12](https://adventofcode.com/2025/day/12) -/-> Solution: [Part 1](./day12/main.cpp) & [Part 2](./day12/main2.cpp)
 
 -->
--- day6/main.cpp
+++ day6/main.cpp
@@ -1,6 +1,50 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <sstream>
+#include <vector>
+#include <unordered_map>
+
+long long add(long long a, long long b) { return a + b; }
+long long sub(long long a, long long b) { return a - b; }
+long long mul(long long a, long long b) { return a * b; }
+long long divi(long long a, long long b) { return a / b; }
+
+std::unordered_map<char, long long (*)(long long, long long)> op = {
+    { '+', add },
+    { '-', sub },
+    { '*', mul },
+    { '/', divi }
+};
+
+bool is_number(const std::string &s){
+    if(s.empty()){
+        return false;
+    }
+    for(char c : s){
+        if(!std::isdigit(static_cast<unsigned char> (c))) return false;
+    }
+    return true;
+}
+
+long long sumOfOperations(std::vector<std::string> numbers, std::vector<char> symbols){
+    int numsPerLine = symbols.size();
+    long long currNumber = 0;
+    long long sumCurrNumber = 0;
+    for(int i = 0; i < numsPerLine; ++i){
+        int j = numsPerLine + i;
+        currNumber = std::stoll(numbers[i]);
+        while(j < numbers.size()){
+            std::cout<< currNumber << " = " << currNumber << " " << symbols[i] << " " << numbers[j] << std::endl;
+            currNumber =  op[symbols[i]](currNumber, std::stoll(numbers[j]));
+            std::cout<< "CURR SUM: " << currNumber <<std::endl;
+            j += numsPerLine;
+        }
+        sumCurrNumber += currNumber;
+    }
+
+    return sumCurrNumber;
+}
 
 
 int main(){
@@ -11,13 +55,47 @@ int main(){
         return 1;
     }
 
+    std::vector<std::string> numbers;
+    std::vector<char> symbols;
+
+    bool symbol_section = false;
+
     std::string line;
     while(std::getline(file, line)){
+        std::stringstream ss(line);
+        std::string token;
+        char symbol;
 
+        while(ss >> token){
+            //std::cout<< "TOKEN: " << token << std::endl;
+            if(!symbol_section){
+                if(is_number(token)){
+                    numbers.push_back(token);
+                }else{
+                    symbol_section = true;
+                    symbol = token[0];
+                    symbols.push_back(symbol);
+                }
+            }else{
+                symbol = token[0];
+                symbols.push_back(symbol);
+            }
+        }
+
+        //std::cout<< "LINE: " << line << std::endl;
 
     }
 
     file.close();
+
+    std::cout << "FINAL RESULT: " << sumOfOperations(numbers, symbols) << std::endl;;
+
+    // std::cout << "NUMBERS:\n";
+    // for (auto& n : numbers) std::cout << n << "\n";
+
+    // std::cout << "\nSYMBOLS:\n";
+    // for (auto& s : symbols) std::cout << s << "\n";
+
 
     return 0;
 }
