a80f66e54be19348254d2fd1701575fde5a6dde6
Jonas Sävås <jonas.savas123@gmail.com>
2025-12-27 18:49:46 +0100

    Adds the solution to day 7 part 1

--- day7/main.cpp
+++ day7/main.cpp
@@ -4,46 +4,50 @@
 #include <vector>
 
 void printVector(const std::vector<std::string>& v) {
-    std::cout<<"NEW" <<std::endl;
+    //std::cout<<"NEW" <<std::endl;
     for (const auto& s : v) {
         std::cout << s << '\n';
     }
 }
 
-void sendBeam(int stringPos, int vectorPos, std::vector<std::string> &inputLines){
+void sendBeam(int stringPos, int vectorPos, std::vector<std::string> &inputLines, long long &nrSplits){
     // We send where we want to shoot
     char currChar = inputLines[vectorPos][stringPos];
     if(currChar == '|') return;
 
     if(currChar == '.') inputLines[vectorPos][stringPos] = '|';
     else if(currChar == '^'){
-        sendBeam(stringPos+1, vectorPos, inputLines);
-        sendBeam(stringPos-1, vectorPos, inputLines);
+        ++nrSplits;
+        sendBeam(stringPos+1, vectorPos, inputLines, nrSplits);
+        sendBeam(stringPos-1, vectorPos, inputLines, nrSplits);
     }
-    printVector(inputLines);
 }
 
 
-long long calcBeamSplit(int beamStart, std::vector<std::string> &inputLines){
+long long calcBeamSplit(int beamStart, std::vector<std::string> &inputLines, long long &nrSplits){
     // Check below the beamStart, or currBeam
     // If there is a splitter there, then again check for a splitter next to it
     // If its empty, then put the beam there
 
-    sendBeam(beamStart, 1, inputLines); // Send the first beam under S
-
+    sendBeam(beamStart, 1, inputLines, nrSplits); // Send the first beam under S
     // Skip the first line
-    for(int i = 1; i<inputLines.size(); ++i){
-        for(auto &c : inputLines[i]){
-            
+    for(int i = 1; i<inputLines.size()-1; ++i){
+        for(int j = 0; j < inputLines[i].size(); ++j){
+            char currChar = inputLines[i][j];
+            if(currChar == '|'){
+                sendBeam(j, i+1, inputLines, nrSplits); // Go down one step and send the beam
+            }
         }
+        std::cout<<"Nr splits: " << nrSplits << std::endl;
+        printVector(inputLines);
     }
 
-    return 1;
+    return nrSplits;
 }
 
 
 int main(){
-    std::fstream file("../input1.txt");
+    std::fstream file("../input.txt");
 
     if(!file.is_open()){
         std::cerr << "Could not open file. \n";
@@ -55,6 +59,7 @@ int main(){
     while(std::getline(file, line)){
         inputLines.push_back(line);
     }
+    file.close();
 
     // Find the beam entrance location
     int beamStart = 0;
@@ -66,9 +71,8 @@ int main(){
         }
     }
 
-    long long result = calcBeamSplit(beamStart, inputLines);
-
-    file.close();
-
+    long long result = 0;
+    result = calcBeamSplit(beamStart, inputLines, result);
+    std::cout<< "RESULT: " << result << std::endl;
     return 0;
 }
