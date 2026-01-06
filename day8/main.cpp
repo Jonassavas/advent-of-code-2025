ed53a890f4f5c1c25462ae5da7e3af40f7cd2109
Jonas Sävås <jonas.savas123@gmail.com>
2026-01-06 23:40:06 +0100

    Adds the puzzle input for day 9

--- day8/main.cpp
+++ day8/main.cpp
@@ -4,11 +4,44 @@
 #include <vector>
 #include <sstream>
 
+struct Point{
+    public:
+        std::vector<int> m_pos;
+};
+
+
 /*
     Each row is the position in 3D space
     x, y, z
 */
+int calcShortestDistance(std::vector<std::vector<Point>> connectedBoxes){
+    std::vector<std::vector<Point>> initialBoxes = connectedBoxes;
+    double savedDistance = INT_MAX;
+    
+    for(size_t i = 0; i < connectedBoxes.size(); ++i){
+        for( size_t j = 0; j < points.size(); ++j){
+            // If we are at the same point --> skip
+            if(i == j){
+                continue;
+            }
+            
+            if(connectedBoxes[i].size() >= 2){
+                continue;
+            }
 
+            double distance = std::sqrt(
+                            (points[j].m_pos[0] - points[i].m_pos[0]) * (points[j].m_pos[0] - points[i].m_pos[0]) + 
+                            (points[j].m_pos[1] - points[i].m_pos[1]) * (points[j].m_pos[1] - points[i].m_pos[1]) +
+                            (points[j].m_pos[2] - points[i].m_pos[2]) * (points[j].m_pos[2] - points[i].m_pos[2])
+                        );
+            
+            if(savedDistance > distance){
+                savedDistance = distance;
+            }
+        }
+    }
+    
+}
 
 int main(){
     std::fstream file("../input.txt");
@@ -17,8 +50,8 @@ int main(){
         std::cerr << "Could not open file. \n";
         return 1;
     }
-
-    std::vector<std::vector<int>> boxPositions;
+    // [[P1, P5],[P2, P4]]
+    std::vector<std::vector<Point>> connectedBoxes;
     std::vector<int> boxConnections(0);
 
     std::string line;
@@ -31,14 +64,22 @@ int main(){
         while(std::getline(ss, token, ',')){
             row.push_back(std::stoi(token));
         }
+        
 
         if(row.size() == 3){
-            boxPositions.push_back(row);
+            std::vector<Point> points;
+            points.push_back(Point{row});
+            connectedBoxes.push_back(points);
+        }else{
+            std::cerr<< "Row did not contain the correct elements";
+            return 1;
         }
 
     }
 
     file.close();
+
+
 
     return 0;
 }
